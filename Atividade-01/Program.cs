be20087bfdb3979bb2a037bd21bcf538254f74a9
AnaBollini <ana.bollini@aluno.senai.br>
2025-09-16 12:20:56 -0300

    Atividade para entrega.

---
+++ Atividade-01/Program.cs
@@ -0,0 +1,56 @@
+﻿// Atividade 01
+string nome;
+int idade;
+string cidade;
+string programação;
+string hobbie;
+
+
+Console.WriteLine("Digite seu nome:");
+nome = Console.ReadLine();
+
+Console.WriteLine("Digite sua idade:");
+idade = Convert.ToInt16(Console.ReadLine());
+
+Console.WriteLine("Digite sua cidade:");
+cidade = Console.ReadLine();
+
+Console.WriteLine("Digite a área da programação que mais gosta:");
+programação = Console.ReadLine();
+
+Console.WriteLine("Digite seu hobbie:");
+hobbie = Console.ReadLine();
+
+Console.WriteLine($"Seu nome é {nome}, você tem {idade} anos, reside em {cidade}, sua área da programação preferida é {programação} e seu hobbie é {hobbie} ");
+
+
+// Atividade 02
+
+char letra;
+
+Console.WriteLine("Digite uma letra:");
+letra = Convert.ToChar(Console.ReadLine().ToUpper()); 
+
+Console.WriteLine($"Você digitou a letra: {letra}.");
+
+
+// Atividade 03
+
+int numero1;
+int numero2;
+
+Console.WriteLine("Digite o primeiro número: ");
+numero1 = Convert.ToInt16(Console.ReadLine());
+
+Console.WriteLine("Digite o segundo número: ");
+numero2 = Convert.ToInt16(Console.ReadLine());
+
+int soma = numero1 + numero2;
+
+Console.WriteLine($"A soma entre {numero1} e {numero2} é igual a: {soma} ");
+
+
+
+
+
+
---
+++ ConvertendoValores/Program.cs
@@ -0,0 +1,26 @@
+﻿// Variáveis sem valor inicial
+string nome;
+int idade;
+char sexo;
+double salario;
+
+
+Console.WriteLine("Digite seu nome:");
+nome = Console.ReadLine();
+
+// Convertendo o valor que foi digitado para o tipo Int16 (int)
+Console.WriteLine("Digite sua idade:");
+idade = Convert.ToInt16(Console.ReadLine());
+
+// Convertendo o valor que foi digitado para o tipo Char
+Console.WriteLine("Qual é o seu sexo: M ou F");
+sexo = Convert.ToChar(Console.ReadLine().ToUpper());
+
+// Convertendo o valor que foi digitado para o tipo Double
+Console.WriteLine("Digite seu salário:");
+salario = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine($"O seu nome é {nome}, você tem {idade} anos, seu sexo é {sexo}, e seu salário é: R${salario},00");
+
+
+
